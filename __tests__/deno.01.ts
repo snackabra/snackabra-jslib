eab30e37ecab25d1cff97468d1a78e8ce7e80033
petersmagnusson <peter@magnusson.institute>
2022-09-20 18:17:12 -0700

    sync

--- __tests__/deno.01.ts
+++ __tests__/deno.01.ts
@@ -1,28 +1,114 @@
-"use strict";
-// import { assertEquals } from "https://deno.land/std@0.156.0/testing/asserts.ts";
+
+
+import { assertEquals, assertThrows } from "https://deno.land/std@0.156.0/testing/asserts.ts";
+
 // import { Client } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 // import { delay } from "https://deno.land/std@0.156.0/async/delay.ts";
-exports.__esModule = true;
-var main_01_ts_1 = require("./main.01.ts");
-Deno.test('string returns to same result', function () {
-    var s = '0123456789abcdefghijklmnop';
-    assertEquals((0, main_01_ts_1.ab2str)((0, main_01_ts_1.base64ToArrayBuffer)((0, main_01_ts_1.arrayBufferToBase64)((0, main_01_ts_1.str2ab)(s)))), s);
+
+
+import {
+  ab2str,
+  str2ab,
+  base64ToArrayBuffer,
+  arrayBufferToBase64,
+  getRandomValues,
+//  Snackabra
+} from './main.01.ts';
+
+
+Deno.test('string returns to same result', () => {
+  const s = '0123456789abcdefghijklmnop';
+  assertEquals(ab2str(base64ToArrayBuffer(arrayBufferToBase64(str2ab(s)))), s);
 });
+
+Deno.test('ten random buffers of random length', () => {
+  for (var i = 0; i < 10; i++) {
+    let random_length = new Uint8Array(4);
+    getRandomValues(random_length);
+    let array = new Uint8Array(random_length[0]);
+    getRandomValues(array);
+    // console.log(array);
+    // console.log(arrayBufferToBase64(array));
+    // console.log(base64ToArrayBuffer(arrayBufferToBase64(array)));
+    assertEquals(base64ToArrayBuffer(arrayBufferToBase64(array)), array);
+  }
+});
+
+Deno.test('test invalid chars - should throw', () => {
+  assertThrows(() => {
+    base64ToArrayBuffer(';;;;;');
+  }, Error, 'Failed to decode base64',);
+});
+
+// a handful of semi-challenging strings
+const z = [
+  'abcdef123456',
+  'ABC*#&@^)!_=`|\\',
+  '',
+  '        ',
+  '=============',
+  '\0',
+  '\0\0\0\0\0\0'
+];
+
+Deno.test('some string <-> binary conversions', () => {
+  for (const s0 of z) {
+    let s1 = str2ab(s0);
+    let s2 = ab2str(s1);
+    assertEquals(s0, s2);
+  }
+});
+
+Deno.test('ten random binary strings of random length', () => {
+  for (var i = 0; i < 10; i++) {
+    let random_length = new Uint8Array(4);
+    getRandomValues(random_length);
+    let array = new Uint8Array(random_length[0]);
+    getRandomValues(array);
+    let s1 = ab2str(array);
+    let s2 = str2ab(s1);
+    assertEquals(s2, array);
+  }
+});
+
+Deno.test('some yucky strings', () => {
+  for (const e of z) {
+    let s0 = e;
+    let s1 = str2ab(s0);
+    let s2 = arrayBufferToBase64(s1);
+    let s3 = base64ToArrayBuffer(s2);
+    let s4 = ab2str(s3);
+    if (s0 !== s4) {
+      console.log('Failed conversion of string, details:');
+      console.log(` s0: ${s0}\n s1: ${s1}\n s2: ${s2}\n s3: ${s3}\n s4: ${s4}\n`);
+    }
+    assertEquals(s0, s4);
+  }
+});
+
+
+
+
+
 ////////////////////////////////////////////////////////////////
+
 // Deno.test("url test", () => {
 //   const url = new URL("./foo.js", "https://deno.land/");
 //   assertEquals(url.href, "https://deno.land/foo.js");
 // });
+
 // // Compact form: name and function
 // Deno.test("hello world #1", () => {
 //   const x = 1 + 2;
 //   assertEquals(x, 3);
 // });
+
 // // Compact form: named function.
 // Deno.test(function helloWorld3() {
 //   const x = 1 + 2;
 //   assertEquals(x, 3);
 // });
+
 // // Longer form: test definition.
 // Deno.test({
 //   name: "hello world #2",
@@ -31,11 +117,13 @@ Deno.test('string returns to same result', function () {
 //     assertEquals(x, 3);
 //   },
 // });
+
 // // Similar to compact form, with additional configuration as a second argument.
 // Deno.test("hello world #4", { permissions: { read: true } }, () => {
 //   const x = 1 + 2;
 //   assertEquals(x, 3);
 // });
+
 // // Similar to longer form, with test function as a second argument.
 // Deno.test(
 //   { name: "hello world #5", permissions: { read: true } },
@@ -44,30 +132,38 @@ Deno.test('string returns to same result', function () {
 //     assertEquals(x, 3);
 //   },
 // );
+
 // // Similar to longer form, with a named test function as a second argument.
 // Deno.test({ permissions: { read: true } }, function helloWorld6() {
 //   const x = 1 + 2;
 //   assertEquals(x, 3);
 // });
+
 // Deno.test("async hello world", async () => {
 //   const x = 1 + 2;
+
 //   // await some async task
 //   await delay(100);
+
 //   if (x !== 3) {
 //     throw Error("x should be equal to 3");
 //   }
 // });
+
 // function foo() {
 //   return 5;
 // }
+
 // interface User {
 //   id: number;
 //   name: string;
 // }
+
 // interface Book {
 //   id: number;
 //   title: string;
 // }
+
 // Deno.test("database", async (t) => {
 //   const client = new Client({
 //     user: "user",
@@ -76,6 +172,7 @@ Deno.test('string returns to same result', function () {
 //     port: 5432,
 //   });
 //   await client.connect();
+
 //   // provide a step name and function
 //   await t.step("insert user", async () => {
 //     const users = await client.queryObject<User>(
@@ -84,6 +181,7 @@ Deno.test('string returns to same result', function () {
 //     assertEquals(users.rows.length, 1);
 //     assertEquals(users.rows[0].name, "Deno");
 //   });
+
 //   // or provide a test definition
 //   await t.step({
 //     name: "insert book",
@@ -100,6 +198,7 @@ Deno.test('string returns to same result', function () {
 //     sanitizeResources: true,
 //     sanitizeExit: true,
 //   });
+
 //   // nested steps are also supported
 //   await t.step("update and delete", async (t) => {
 //     await t.step("update", () => {
@@ -107,10 +206,12 @@ Deno.test('string returns to same result', function () {
 //       // and the next test step will run
 //       throw new Error("Fail.");
 //     });
+
 //     await t.step("delete", () => {
 //       // ...etc...
 //     });
 //   });
+
 //   // steps return a value saying if they ran or not
 //   const testRan = await t.step({
 //     name: "copy books",
@@ -119,6 +220,7 @@ Deno.test('string returns to same result', function () {
 //     },
 //     ignore: true, // was ignored, so will return `false`
 //   });
+
 //   // steps can be run concurrently if sanitizers are disabled on sibling steps
 //   const testCases = [1, 2, 3];
 //   await Promise.all(testCases.map((testCase) =>
@@ -132,5 +234,6 @@ Deno.test('string returns to same result', function () {
 //       sanitizeExit: false,
 //     })
 //   ));
+
 //   client.end();
 // });
--- __tests__/main.01.ts
+++ __tests__/main.01.ts
@@ -7,7 +7,7 @@
  * ****************************************************************/
 
 
-function _sb_exception(loc, msg) {
+function _sb_exception(loc: string, msg: string) {
   const m = '<< SB lib error (' + loc + ': ' + msg + ') >>';
   // for now disabling this to keep node testing less noisy
   // console.error(m);
@@ -20,7 +20,7 @@ let _ws = WebSocket;
 /** 
  * Fills buffer with random data
  */
-function getRandomValues(buffer: Uint8Array) {
+export function getRandomValues(buffer: Uint8Array) {
   return _crypto.getRandomValues(buffer);
 }
 
@@ -39,7 +39,7 @@ const b64_regex = new RegExp('^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-
  * Returns 'true' if (and only if) string is well-formed base64.
  * Works same on browsers and nodejs.
  */
-function _assertBase64(base64) {
+function _assertBase64(base64: string) {
   /* webpack 4 doesn't support optional chaining, requires webpack 5;
      since wp 5 is pretty recent (2020-10-10), we'll avoid using
      optional chaining in this library for a while */
@@ -55,7 +55,7 @@ function _assertBase64(base64) {
  * @param {string} string
  * @return {Uint8Array} buffer
  */
-function str2ab(string) {
+export function str2ab(string: string) {
   const length = string.length;
   const buffer = new Uint8Array(length);
   for (let i = 0; i < length; i++) buffer[i] = string.charCodeAt(i);
@@ -70,7 +70,50 @@ function str2ab(string) {
  * @return {Uint8Array} buffer
  *
  */
-function ab2str(buffer) {
+export function ab2str(buffer: Uint8Array) {
   if (!_assertUint8Array(buffer)) _sb_exception('ab2str()', 'parameter is not a Uint8Array buffer'); // this will throw
-  return String.fromCharCode.apply(null, new Uint8Array(buffer));
+  // return String.fromCharCode.apply(String, new Uint8Array(buffer));
+  return new TextDecoder("utf-8").decode(buffer);
 }
+
+
+/*
+ * Historically we wanted detailed controll of this, but looks like
+ * we no longer need that. Should the need arise, a good choice is
+ * https://raw.githubusercontent.com/dankogai/js-base64/main/base64.mjs
+ */
+
+/**
+ * Standardized 'btoa()'-like function, e.g., takes a binary string
+ * ('b') and returns a Base64 encoded version ('a' used to be short
+ * for 'ascii').
+ *
+ * @param {buffer} Uint8Array buffer
+ * @return {string} base64 string
+ */
+export function arrayBufferToBase64(buffer: Uint8Array) {
+  return window.btoa(unescape(encodeURIComponent(new TextDecoder("utf-8").decode(buffer))));
+}
+
+/**
+ * Standardized 'atob()' function, e.g. takes the a Base64 encoded
+ * input and decodes it. Note: always returns Uint8Array.
+ *
+ * @param {string} base64 string
+ * @return {Uint8Array} returns decoded result
+ */
+export function base64ToArrayBuffer(asc: string) {
+  return new TextEncoder().encode(window.atob(asc));
+}
+
+export function _appendBuffer(buffer1: Uint8Array, buffer2: Uint8Array) {
+  try {
+    const tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+    tmp.set(new Uint8Array(buffer1), 0);
+    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+    return tmp.buffer;
+  } catch (e) {
+    console.error(e);
+    return {};
+  }
+};
