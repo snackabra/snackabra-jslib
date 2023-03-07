27d733c4a5c3ea5232014b14387ea91e6f3f61a4
Peter M <psm@384.co>
2023-03-06 21:26:09 -0800

    cleaned up some webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     entry: './snackabra.js', // the entry point of your application
+    mode: 'production',
     optimization: {
         minimize: true
     },
     output: {
         library: 'SB', // the name exported to window
         libraryTarget: 'umd',
-        filename: 'bundle.js'
+        filename: 'snackabra.min.js',
+        path: __dirname + '/'
     }
 };
